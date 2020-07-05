5bdf6f23fd5a09715af71bd5f36f07d5b2a9b6ca
Z. Saygın Doğu <saygindogu@gmail.com>
2020-07-05 13:48:58 +0300

    [Stuff] Some stuff

---
+++ ArgonAssault/Assets/Scripts/CollusionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollusionHandler : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        deathFX.SetActive(true);
+        Instantiate(deathFX, transform.position, Quaternion.identity);
+        print("Player dead");
+        SendMessage("OnPlayerDeath");
+    }
+}
