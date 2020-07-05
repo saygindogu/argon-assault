26f940641229c848a5a63454d647c157bbe89a93
Z. Saygın Doğu <saygindogu@gmail.com>
2020-07-05 12:38:02 +0300

    [CleanCode] Some improvements

--- ArgonAssault/Assets/Scripts/MusicPlayer.cs
+++ ArgonAssault/Assets/Scripts/MusicPlayer.cs
@@ -1,21 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-
     void Awake(){
         Object.DontDestroyOnLoad(gameObject);   
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        Invoke("LoadFirstScene", 3f);
-    }
-
-    private void LoadFirstScene(){
-        SceneManager.LoadScene(1);
     }
 }
